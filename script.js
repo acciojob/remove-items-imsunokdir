7ec1ab764f12ac5996066add229010dfb9c3c04c
imsunokdir <67774734+imsunokdir@users.noreply.github.com>
2024-07-22 11:46:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+const button = document.querySelector('input[type="button"]');
+
+button.addEventListener('click',()=>{
+	const select = document.getElementById('colorSelect');
+	const selectedItem = select.selectedIndex;
+
+	if(selectedItem != -1){
+		select.remove(selectedItem)
+	}
+})
